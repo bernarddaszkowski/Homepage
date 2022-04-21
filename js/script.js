f2d93f6986e2bf7607de1083d0d4ab8a9b6a3123
Bernard Daszkowski <bernard.daszkowski@gmail.com>
2022-04-21 12:28:35 +0100

    replan JS, adding new function

--- js/script.js
+++ js/script.js
@@ -1,24 +1,27 @@
 {
-function welcome() {
-    console.log("Witam deweloperów zaglądających na moją pierwszą stronę.!");    
-}
-welcome();
-
-const button = document.querySelector(".table__button");
-const table = document.querySelector(".table");
-const buttonName = document.querySelector(".js-buttonName");
-
-const tableDisplayOn = () => {
-    table.classList.toggle("table--display");
-
-    if (table.classList.contains("table--display")) {
-        buttonName.innerText = "Wyłącz";
+    function welcome() {
+        console.log("Witam deweloperów zaglądających na moją pierwszą stronę.!");
     }
-    else {
-        buttonName.innerText = "Włącz";
-    };
-}
+    welcome();
 
-button.addEventListener("click", tableDisplayOn);
+    const tableDisplayOn = () => {
+        const buttonName = document.querySelector(".js-buttonName");
+        const table = document.querySelector(".table");
+        const button = document.querySelector(".table__button");
+        table.classList.toggle("table--display");
+        if (table.classList.contains("table--display")) {
+            buttonName.innerText = "Wyłącz";
+        }
+        else {
+            buttonName.innerText = "Włącz";
+        };
+    }
+    const init = () => {
+        const button = document.querySelector(".table__button");
+        button.addEventListener("click", tableDisplayOn);
+    }
+
+    init();
+
 }
 
